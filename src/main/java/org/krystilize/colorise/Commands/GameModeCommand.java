366f31ebd50535e402b74a3c11f716c9843ee16f
KrystilizeNevaDies <tyreece@rozycki.family>
2024-08-10 12:32:55 +1000

    temporarily refactor gamemode command

--- src/main/java/org/krystilize/colorise/Commands/GameModeCommand.java
+++ src/main/java/org/krystilize/colorise/Commands/GameModeCommand.java
@@ -1,9 +1,10 @@
-package org.krystilize.colorise.Commands;
+package org.krystilize.colorise.commands;
 
 import net.minestom.server.command.CommandSender;
 import net.minestom.server.command.builder.Command;
 import net.minestom.server.command.builder.CommandContext;
-import net.minestom.server.command.builder.arguments.ArgumentType;
+import net.minestom.server.command.builder.arguments.ArgumentEnum;
+import static net.minestom.server.command.builder.arguments.ArgumentType.*;
 import net.minestom.server.entity.GameMode;
 import net.minestom.server.entity.Player;
 import org.krystilize.colorise.Util;
@@ -15,19 +16,21 @@ public class GameModeCommand extends Command {
         setCondition((sender, s) -> sender instanceof Player player && Util.ADMINS.contains(player.getUsername()));
 
         addSyntax(this::executeNoArgs);
-        addSyntax(this::executeArgs, ArgumentType.Enum("gamemode", GameMode.class));
+        addSyntax(this::executeArgs, Enum("gamemode", GameMode.class).setFormat(ArgumentEnum.Format.LOWER_CASED));
     }
 
     private void executeNoArgs(CommandSender sender, CommandContext context) {
         switch (context.getInput()) {
             case "gms", "gma" -> execute(sender, GameMode.ADVENTURE);
             case "gmc" -> execute(sender, GameMode.CREATIVE);
+            default -> sender.sendMessage("Invalid gamemode!");
         }
     }
 
     private void executeArgs(CommandSender sender, CommandContext context) {
         GameMode gamemode = context.get("gamemode");
-        execute(sender, gamemode);
+        if (!(sender instanceof Player player)) return;
+        player.setGameMode(gamemode);
     }
 
     private void execute(CommandSender sender, GameMode gamemode) {
